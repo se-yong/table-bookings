b09f3ecbedff193ed6a2d347beca391ae3e71a11
se-yong <parksae0428@gmail.com>
2022-04-20 21:57:40 +0900

    회원기능 구현 / 회원가입,로그인 뷰

--- table_bookings/web/models.py
+++ table_bookings/web/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+    nickname = models.CharField(null=False, max_length=20)
+    profile_image = models.ImageField(upload_to='uploads/%Y/%m/%d/', null=True)
