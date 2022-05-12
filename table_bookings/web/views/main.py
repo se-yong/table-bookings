edb843c6492ef33501fe98bb152fab3ba503d542
se-yong <parksae0428@gmail.com>
2022-05-13 05:49:16 +0900

    [ADD]: 추천 식당 페이지와 검색기능 구현

--- table_bookings/web/views/main.py
+++ table_bookings/web/views/main.py
@@ -1,5 +1,9 @@
 from django.views.generic import TemplateView
-from ..models import Recommendation
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.shortcuts import get_object_or_404
+
+from ..models import Recommendation, Restaurant, Category
 
 
 class IndexView(TemplateView):
@@ -9,5 +13,36 @@ class IndexView(TemplateView):
         recommendations = Recommendation.objects.filter(visible=True).order_by('sort')\
                               .select_related('restaurant').all()[:4]
         return {
-            'recommendations' : recommendations
+            'recommendations': recommendations
         }
+
+
+class SearchView(TemplateView):
+    template_name = 'main/search.html'
+
+    def get_context_data(self, **kwargs):
+        page_number = self.request.GET.get('page', '1')
+        keyword = self.request.GET.get('keyword')
+        category_id = self.request.GET.get('category')
+
+        category = None
+
+        query_sets = Restaurant.objects.filter(visible=True).order_by('-created_at')
+        if keyword:
+            query_sets = query_sets.filter(Q(name__istartswith=keyword) | Q(address__istartswith=keyword))
+        if category_id:
+            category = get_object_or_404(Category, id=int(category_id))
+            query_sets = query_sets.filter(category=category)
+
+        restaurants = query_sets.all()
+        paginator = Paginator(restaurants, 12)
+
+        paging = paginator.get_page(page_number)
+
+        return {
+            'paging': paging,
+            'selected_keyword': keyword,
+            'selected_category': category
+        }
+
+
