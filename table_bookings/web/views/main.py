87d061045b4e25bfb03b9dbaca9c12faff1d16ee
se-yong <parksae0428@gmail.com>
2022-05-13 17:06:11 +0900

    [ADD]: 복잡한 조건의 검색 기능 구현(날짜,요일)

--- table_bookings/web/views/main.py
+++ table_bookings/web/views/main.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.views.generic import TemplateView
 from django.core.paginator import Paginator
 from django.db.models import Q
@@ -25,6 +27,10 @@ class SearchView(TemplateView):
         keyword = self.request.GET.get('keyword')
         category_id = self.request.GET.get('category')
 
+        weekday = self.request.GET.get('weekday')
+        start_time = self.request.GET.get('start')
+        end_time = self.request.GET.get('end')
+
         category = None
 
         query_sets = Restaurant.objects.filter(visible=True).order_by('-created_at')
@@ -34,7 +40,31 @@ class SearchView(TemplateView):
             category = get_object_or_404(Category, id=int(category_id))
             query_sets = query_sets.filter(category=category)
 
-        restaurants = query_sets.all()
+        relation_conditions = None
+
+        if weekday:
+            # SELECT * FROM Restaurant r INNER JOIN RestaurantTable rt ON rt.restaurant_id = r.id
+            # WHERE rt.weekday = :weekday
+            relation_conditions = Q(restauranttable__weekday=weekday)
+
+        if start_time:
+            start_time = datetime.time.fromisoformat(start_time) # 12:00:00
+            if relation_conditions:
+                relation_conditions = relation_conditions & Q(restauranttable__time__gte=start_time)
+            else:
+                relation_conditions = Q(restauranttable__time__gte=start_time)
+
+        if end_time:
+            end_time = datetime.time.fromisoformat(end_time)  # 12:00:00
+            if relation_conditions:
+                relation_conditions = relation_conditions & Q(restauranttable__time__lte=end_time)
+            else:
+                relation_conditions = Q(restauranttable__time__lte=end_time)
+
+        if relation_conditions:
+            query_sets = query_sets.filter(relation_conditions)
+
+        restaurants = query_sets.distinct().all()
         paginator = Paginator(restaurants, 12)
 
         paging = paginator.get_page(page_number)
@@ -42,7 +72,10 @@ class SearchView(TemplateView):
         return {
             'paging': paging,
             'selected_keyword': keyword,
-            'selected_category': category
+            'selected_category': category,
+            'selected_weekday': weekday,
+            'selected_start': datetime.time.isoformat(start_time) if start_time else '',
+            'selected_end': datetime.time.isoformat(end_time) if end_time else ''
         }
 
 
